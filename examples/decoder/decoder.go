cd96bd76b532dcd4180b28d5c230bb7c8b9a4b59
hidu <duv123+github@gmail.com>
2021-06-05 21:47:58 +0800

    go_fmt

--- examples/decoder/decoder.go
+++ examples/decoder/decoder.go
@@ -1,8 +1,6 @@
-/*
- * Copyright(C) 2020 github.com/hidu  All Rights Reserved.
- * Author: hidu (duv123+git@baidu.com)
- * Date: 2020/11/7
- */
+// Copyright(C) 2020 github.com/hidu  All Rights Reserved.
+// Author: hidu (duv123+git@baidu.com)
+// Date: 2020/11/7
 
 // go build -buildmode=plugin
 
